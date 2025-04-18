e2a5dcabcf096be9b2b6eeb6bdd1115cde4397c1
d16-nichevo <nichevo@iinet.net.au>
2025-04-19 00:05:37 +0800

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,220 @@
+﻿namespace FoundryPlaylistConverter
+{
+    internal class Program
+    {
+        // All alpha and numeric characters that can be
+        // used to make up a Foundry ID (A-Z, a-z, 0-9).
+        private const string IdChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        private static Random Random = new Random();
+        private static List<string> MusicFiles = new List<string>();
+
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Sanity check args:
+                if (args.Length != 3)
+                {
+                    // Bad args. Show usage info:
+                    Usage();
+                    Environment.Exit(1);
+                }
+                // Do work if args are okay:
+                ReadPlaylist(args[1]);
+                WriteJson(args[0], args[2]);
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                // Last-chance exception handler:
+                Console.WriteLine($"ERROR: Unhandled expcetion:");
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(1);
+            }
+        }
+
+        /// <summary>
+        /// Reads a M3U playlist or text file full of filenames.
+        /// </summary>
+        /// <param name="path">The path to the file to read.</param>
+        private static void ReadPlaylist(string path)
+        {
+            // Bail if file doesn't exist:
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException(path);
+            }
+
+            // Read each line:
+            foreach (string line in File.ReadLines(path))
+            {
+                if (line.StartsWith("file://"))
+                {
+                    // This seems to be a file protocol line (as is common in M3Us).
+                    // Treat it accordingly. Note: it is URL-encoded.
+                    // Example: file:///C:/Music/Games/Game%20123/track%20five.mp3
+                    Uri u = new Uri(line);
+                    // LocalPath gets us the OS-specific, non-file-protocol path.
+                    if (File.Exists(u.LocalPath))
+                    {
+                        MusicFiles.Add(u.LocalPath);
+                        // Console.WriteLine($"Found: {u.LocalPath}");
+                    }
+                }
+                else if (File.Exists(line))
+                {
+                    // This is not a file protocol line.
+                    // See if it's a file, ignore otherwise:
+                    MusicFiles.Add(Path.GetFullPath(line));
+                    // Console.WriteLine($"Found: {line}");
+                }
+            }
+
+            // If we didn't find any music files, something went wrong:
+            if (MusicFiles.Count == 0)
+            {
+                throw new InvalidDataException($"Found no files inside playlist: {path}");
+            }
+        }
+
+        /// <summary>
+        /// Writes the list of files to a JSON format
+        /// understandable by Foundry.
+        /// </summary>
+        /// <param name="userDataPath">The user data directory used by Foundry.</param>
+        /// <param name="outputPath">The path to the JSON file to write.</param>
+        private static void WriteJson(string userDataPath, string outputPath)
+        {
+            // Bail out if the specified directory isn't real:
+            if (!Directory.Exists(userDataPath))
+            {
+                throw new DirectoryNotFoundException(userDataPath);
+            }
+
+            // Write to the output file:
+            using (StreamWriter outputFile = new StreamWriter(outputPath))
+            {
+                // First, write "preamble" stuff:
+                outputFile.WriteLine(
+$@"{{
+  ""folder"": ""{GetId()}"",
+  ""name"": ""{Path.GetFileNameWithoutExtension(outputPath)}"",
+  ""sounds"": [");
+
+                // Then write JSON data for each file in the list:
+                for (int i = 0; i < MusicFiles.Count; i++)
+                {
+                    outputFile.WriteLine(GenerateJsonSoundArrayElement(MusicFiles[i], userDataPath, i != MusicFiles.Count - 1));
+                }
+
+                // Finally, write "afterwards" stuff:
+                outputFile.WriteLine(
+$@"],
+  ""channel"": ""music"",
+  ""mode"": 1,
+  ""playing"": false,
+  ""fade"": null,
+  ""sorting"": ""a"",
+  ""seed"": 0,
+  ""flags"": {{
+    ""exportSource"": {{
+      ""world"": ""foundry-playlist-convertor"",
+      ""system"": ""pf2e"",
+      ""coreVersion"": ""12.331"",
+      ""systemVersion"": ""6.11.1""
+    }}
+  }},
+  ""_stats"": {{
+    ""coreVersion"": ""12.331"",
+    ""systemId"": ""pf2e"",
+    ""systemVersion"": ""6.8.5"",
+    ""createdTime"": {DateTimeOffset.Now.ToUnixTimeSeconds()},
+    ""modifiedTime"": {DateTimeOffset.Now.ToUnixTimeSeconds()},
+    ""lastModifiedBy"": ""{GetId()}""
+  }},
+  ""description"": """"
+}}
+");
+            }
+
+            // Note result to user:
+            Console.WriteLine($"SUCCESS: Wrote {MusicFiles.Count} items to {outputPath}");
+        }
+
+        /// <summary>
+        /// Write a chunk of Foundry-compatible JSON for a single audio file.
+        /// </summary>
+        /// <param name="soundPath">The path to the audio file, as taken from a playlist.</param>
+        /// <param name="userDataPath">The user data directory used by Foundry.</param>
+        /// <param name="trailingComma">Whether to put a trailing comma. Usually this is true except for the last item.</param>
+        private static string GenerateJsonSoundArrayElement(string soundPath, string userDataPath, bool trailingComma)
+        {
+            string name = Path.GetFileNameWithoutExtension(soundPath);
+            string path = Path.GetRelativePath(userDataPath, soundPath).Replace('\\', '/');
+            string id = GetId();
+            string comma = trailingComma ? "," : String.Empty;
+            return $@"    {{
+      ""name"": ""{name}"",
+      ""path"": ""{path}"",
+      ""_id"": ""{id}"",
+      ""channel"": ""music"",
+      ""playing"": false,
+      ""pausedTime"": null,
+      ""repeat"": false,
+      ""volume"": 0.5,
+      ""fade"": null,
+      ""sort"": 0,
+      ""flags"": {{}}
+    }}{comma}";
+        }
+
+        /// <summary>
+        /// Generate a Foundry-compatible ID.
+        /// </summary>
+        /// <returns>
+        /// This is fully random. Is this how Foundry does it?
+        /// Probably not. But it works.
+        /// </returns>
+        private static string GetId()
+        {
+            var stringChars = new char[16];
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = IdChars[Random.Next(IdChars.Length)];
+            }
+            return new String(stringChars);
+        }
+
+        /// <summary>
+        /// Display usage information.
+        /// </summary>
+        private static void Usage()
+        {
+            string exeName;
+            try
+            {
+                exeName = Path.GetFileNameWithoutExtension(Environment.ProcessPath)!.ToUpper();
+            }
+            catch
+            {
+                exeName = "FOUNDRYPLAYLISTCONVERTOR";
+            }
+            Console.WriteLine(
+$@"Converts a list of files or M3U playlist into a Playlist JSON importable by Foundry VTT.
+
+For more information, visit:
+https://github.com/d16-nichevo/FoundryPlaylistConverter
+
+{exeName} [userdataroot] [playlist] [output]
+
+  userdataroot  Path to foundry's data root.  
+  playlist      Path to the input list.
+  output        Path to write the JSON output."
+            );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# FoundryPlaylistConverter
-Convert M3U playlists or lists of files to Foundry-importable JSON for Foundry playlists.
+﻿# About This Tool
+
+This is a command-like tool that will take either:
+
+* `M3U` [playlist](https://en.wikipedia.org/wiki/M3U)
+  * Many audio players can create `M3U` playists, including FooBar and VLC Player.
+* A text file containing a list of filenames.
+
+... and it will convert that file to ...
+
+* A `JSON` file compatible with [Foundry VTT](https://foundryvtt.com/). Specifically, Foundry's Playlists feature.
+
+I created this tool because it's much easier to work with playlists inside audio player software than it is to work with the Playlists in Foundry.
+
+# ⚠️ Warning
+
+I created this tool for my own use. It is not highly-polished. Note that:
+
+1. It has not been extensively tested.
+   * This tool theoretically should be able to run on Linux, but I use a Windows machine, and have not tested on Linux at all.
+1. It probably contains bugs.
+1. I haven't gone to great efforts to make it user-friendly.
+
+If this tool proves to be popular I may refine it. If you'd like to express interest, see the contact section at the bottom of this readme.
+
+# Installation
+
+(to be filled in)
+
+# Usage
+
+Using the command line is out-of-scope for this readme. If you need help, there are guides on the internet. [Here is one.](https://www.freecodecamp.org/news/how-to-use-the-cli-beginner-guide/)
+
+The usage information will appear if you use the tool without correct arguments:
+
+```
+Converts a list of files or M3U playlist into a Playlist JSON importable by Foundry VTT.
+
+For more information, visit:
+https://github.com/nichevo/blah
+
+FOUNDRYPLAYLISTCONVERTOR [userdataroot] [playlist] [output]
+
+  userdataroot  Path to foundry's data root.
+  playlist      Path to the input list.
+  output        Path to write the JSON output.
+```
+
+⚠️ Remember: it's good practise to enclose each argument in quotes `"`.
+
+An example valid command line to use this tool:
+
+* `FoundryPlaylistConvertor "C:/Documents/Foundry App Data/Data" "C:/playlists/combat-music.m3u" "C:/downloads/foundry-combat-playlist.json"`
+
+## `userdataroot` Argument
+
+This is the [User Data location for Foundry](https://foundryvtt.com/article/user-data/).
+
+If you're not sure where this is:
+
+1. Open Foundry.
+1. On the main screen, open the settings in the top-right.
+1. Look for a setting called **User Data Path**.
+   * On my computer (Windows) the value is `C:/Documents/Foundry App Data`
+
+The `userdataroot` argument is as per this value, but with `/Data` added. For example:
+
+* `C:/Documents/Foundry App Data/Data`
+
+# `playlist` Argument
+
+This is the full path to the file to read from. (It may be possible to use a relative path, I didn't test.)
+
+The file can either be:
+
+1. A valid [M3U file](https://en.wikipedia.org/wiki/M3U).
+   * Many audio players can create `M3U` playists, including FooBar and VLC Player.
+1. A text file where each line contains a full path to an audio file.
+   * It shouldn't be a problem if the file contains lines that non-path content. But don't include files that aren't audio files.
+
+⚠️ Important: All files in the playlist or text file list must be somewhere inside the `userdataroot` directory. Foundry simply will not serve files from outside the User Data location.
+
+So if your `userdataroot` is `C:/Documents/Foundry App Data/Data`:
+
+* Your playlist could contain the file `C:/Documents/Foundry App Data/Data/Music/Combat/track1.mp3`
+* Your playlist should NOT contain the file `C:/My Music/Combat/track1.mp3`
+
+## `output` Argument
+
+This is simply the full path to where you want the `JSON` file to be written.
+
+Ideally you should name the file with a `.JSON` extension, but you don't have to.
+
+# Contact the Author
+
+If you find this tool useful, and you'd like to see it better-developed, please let me know.
+
+* https://deck16.net/contact
